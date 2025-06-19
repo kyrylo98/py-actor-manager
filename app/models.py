a1fe6f75870b2983c105110609ffa5cc48695103
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-06-19 18:12:33 +0200

    Using CRUD

--- app/models.py
+++ app/models.py
@@ -1,4 +1,10 @@
 from dataclasses import dataclass
 
 
-# add dataclass here
+@dataclass
+class Actor:
+    def __init__(self, id: int, first_name: str, last_name: str) -> None:
+        self.id = id
+        self.firstName = first_name
+        self.last_name = last_name
+
