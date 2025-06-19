3b894f2fdb2c6dcf52bf90d96f63749707f04ae1
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-06-19 18:39:38 +0200

    Using CRUD

--- app/managers.py
+++ app/managers.py
@@ -5,9 +5,10 @@ from app.models import Actor
 
 class ActorManager:
 
-    def __init__(self, db_name: str) -> None:
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self._conn = sqlite3.connect(db_name)
+        self.table_name = table_name
         self._conn.execute(f"""
             CREATE TABLE IF NOT EXISTS {self.table_name} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,8 +20,8 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> Actor:
         cursor = self._conn.execute(
-            f"INSERT INTO {self.table_name}"
-            f" (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name)
         )
         self._conn.commit()
@@ -35,13 +36,15 @@ class ActorManager:
     def update(self, pk: int, new_first_name: str,
                new_last_name: str) -> None:
         self._conn.execute(
-            f"UPDATE {self.table_name} SET first_name = ?,"
-            f" last_name = ? WHERE id = ?",
+            f"UPDATE {self.table_name} SET first_name = ?, "
+            f"last_name = ? WHERE id = ?",
             (new_first_name, new_last_name, pk)
         )
         self._conn.commit()
 
     def delete(self, pk: int) -> None:
-        self._conn.execute(f"DELETE FROM {self.table_name} WHERE id = ?",
-                           (pk,))
+        self._conn.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (pk,)
+        )
         self._conn.commit()
